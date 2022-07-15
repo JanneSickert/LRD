8f22f3044c75f2edf698be110b42ee02741af3c4
Sickert <janne.sickert@irix.ch>
2022-07-15 14:49:31 +0200

    Konstanten eingefuert

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from LinearRegressionData import LinearRegressionData
+from Konstanten import Konstanten
+
+CONST = Konstanten()
 
 if __name__ == '__main__':
-    data = LinearRegressionData({"feature" : "T3", "target" : "T4"}, "data.csv")
+    data = LinearRegressionData({"feature" : "T3", "target" : "T4"}, CONST.get_path_to_csv())
     print(data)
