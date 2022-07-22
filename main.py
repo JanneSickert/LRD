f3906ff8f37212dbaaa9d0d87afaafb299ae445d
Sickert <janne.sickert@irix.ch>
2022-07-23 01:39:27 +0200

    Richtung mit winkeln errechnen

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import IsNot
 from LinearRegressionData import LinearRegressionData
 from Konstanten import Konstanten
 import numpy as np
@@ -32,11 +33,30 @@ def create_formula(a, d, min_value):
     formular_string = "f(x) = " + min_value["plus_or_minus"] + " " + str(a) + " * x " + str(d)
     return formular_string
 
-def show_graph(a, d, data):
+def make_my_values(a, d, x):
+    return (a*x+d)
+
+def show_graph(data, min_value, a = 1, x = 1, d = 0):
     plt.figure()
-    plt.plot(data.feature["data"], test_function(a, data.feature["data"], d))
+    plt.title(create_formula(a, d, min_value))
+    foooo = [data.feature["data"][length - 1], make_my_values(a, d, x)]
+    plt.plot([[0, d], foooo])
     plt.scatter(data.feature["data"], data.target["data"])
     plt.show()
+
+def winkel_berechnen(average_increase, data, max_feature):
+    # --------------------------------------
+    # cos(α) = (Ankathete / Hypotenuse)
+    # -------------------------------------
+        i = 0
+        cos = []
+        while i < data.target["data"].size:
+            t, f = data.target["data"][i] ** 2, data.feature["data"][i] ** 2
+            e = max_feature / (math.sqrt(t) + math.sqrt(f))
+            print(average_increase)
+            cos.append(e)
+            i = i + 1
+        return cos
 
 if __name__ == '__main__':
     print("start LRD")
@@ -46,38 +66,39 @@ if __name__ == '__main__':
     min_value = {"index": 0, "value": math.inf, "plus_or_minus": None}  # min_value object
     min_value["value"] = calculate_distance(1, mittelwert, data)    # y = x + d
     average_increase = calculate_average_increase(data)
-    max_target = max(data.target["data"])
+    max_target, max_feature = max(data.target["data"]), max(data.feature["data"])
     step_d = max_target / average_increase / CONST.get_accuracy()
-    step_a = average_increase / max_target
+    steps_a = winkel_berechnen(average_increase, data, max_feature)
     max_increased = average_increase / CONST.get_accuracy()
     b = True
     step = 0
     while b:    # function rise
-        a, d = step_a * step, mittelwert - (step * step_d)
+        a, d = steps_a[step] * step, mittelwert - (step * step_d)
         diffrent = calculate_distance(a, d, data)
         if diffrent < min_value["value"]:
             min_value["value"] = diffrent
             min_value["index"] = step
             min_value["plus_or_minus"] = "+"
-        if step == 1000:
+        if step <= len(steps_a):
             b = False
         else:
             step = step + 1
     b = True
     step = 0
     while b:    # function fall
-        a, d = step_a * step, mittelwert - (step * step_d)
+        a, d = steps_a[step] * step, mittelwert - (step * step_d)
         diffrent = calculate_distance(a, d, data)
         if diffrent < min_value["value"]:
             min_value["value"] = diffrent
             min_value["index"] = step
             min_value["plus_or_minus"] = "-"
-        if step == -1000:
+        if step >= ((-1) * len(steps_a)):
             b = False
         else:
             step = step - 1
-    a, d = step_a * min_value["index"], mittelwert - (min_value["index"] * step_d)
     formula = create_formula(a, d, min_value)
-    show_graph(a, d, data)
+    pa, pd = steps_a[min_value["index"]] * step, mittelwert - (min_value["index"] * step_d)
+    px = data.feature["data"][min_value["index"]]
+    show_graph(data, min_value, a = pa, x = pa, d = pd)
     print(formula)
     print("end LRD")
