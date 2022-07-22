b6e9ef66825ceb4163c0daefd9206454124e4296
Sickert <janne.sickert@irix.ch>
2022-07-22 09:37:00 +0200

    d

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def test_function(a, x, d):
     return (np.add(np.multiply(x, a), d))
 
 def calculate_distance(a, d, data):
-    return np.sum(np.sqrt(np.power(np.subtract(test_function(a, data.feature["data"], d), data.target["data"]))))
+    return (np.sum(np.sqrt(np.power(np.subtract(test_function(a, data.feature["data"], d), data.target["data"])), 2)))
 
 def calculate_mittelwert(data):
     summe = np.sum(data.target["data"])
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     length = data.target["data"].size
     mittelwert = calculate_mittelwert(data)
     min_value = {"index": 0, "value": math.inf, "plus_or_minus": None}  # min_value object
-    min_value["value"] = calculate_distance(1, mittelwert, data)    # y = x + d
+    min_value["value"] = calculate_distance([1], [mittelwert], data)    # y = x + d
     average_increase = calculate_average_increase(data)
     max_target = max(data.target["data"])
     step_d = max_target / average_increase / CONST.get_accuracy()
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         else:
             step = step + 1
     b = True
-    step = -1
+    step = 0
     while b:    # function fall
         a, d = step_a * step, mittelwert - (step * step_d)
         diffrent = calculate_distance(a, d, data)
